76c3d417c221a5770c5b0e6c26d4a5b184edbdf7
Mohd. Jahidul Islam <jahidjob5@outlook.com>
2024-06-28 22:35:01 +0300

    [3] Form:- getInputValue

--- README.md
+++ README.md
@@ -1,15 +1,22 @@
+
+# FaceHook
 # React + Vite
 
-[commit-1]
-npm i @tailwindcss/typography
-    => Tailwind CSS that provides a set of typographic defaults and helper classes to style typography easily.
+[commit-1] <br>
+npm i @tailwindcss/typography <br> 
+    => Tailwind CSS that provides a set of typographic defaults and helper classes to style typography easily. <br>
 
-npm i react router-dom
-    => set link to Components
+npm i react router-dom <br>
+    => set link to Components <br>
 
 
-[commit-2]
-npm i react-hook-form
-    => control Userr Acount Crreation.
-    => called r-hook-form
-    => make dynamic from control for users reg value.
+[commit-2] <br>
+npm i react-hook-form <br>
+    => control Userr Acount Crreation. <br>
+    => called r-hook-form <br>
+    => make dynamic Form Control for users reg value. <br>
+
+[commit-3] <br>
+    => reqiured value <br>
+    => reqired Error
+    => get user Valu by click <br>
--- src/components/auth/Loginform.jsx
+++ src/components/auth/Loginform.jsx
@@ -1,10 +1,11 @@
 import { useForm } from "react-hook-form";
+import Field from "../common/Field";
 
 const Loginform = () => {
   const {
     register,
     handleSubmit,
-    formState: { error },
+    formState: { errors },
   } = useForm();
 
   const submitForm = (formData) => {
@@ -16,7 +17,44 @@ const Loginform = () => {
       <form
         onSubmit={handleSubmit(submitForm)}
         className="border-b border-[#3F3F3F] pb-10 lg:pb-[60px]"
-      ></form>
+      >
+        <Field label="Email" error={errors.email}>
+          <input
+            {...register("email", { required: "Required Email" })}
+            type="email"
+            name="email"
+            id="email"
+            className={`auth-input ${
+              !!errors.email ? "border-red-500" : "border-gray-500"
+            }`}
+          ></input>
+        </Field>
+        <Field label="Password" error={errors.password}>
+          <input
+            {...register("password", {
+              required: "Required password",
+              minLength: {
+                value: 8,
+                message: "Your Password Must be 8 Characterrs",
+              },
+            })}
+            type="password"
+            name="password"
+            id="password"
+            className={`auth-input ${
+              !!errors.password ? "border-red-500" : "border-gray-500"
+            }`}
+          ></input>
+        </Field>
+        <Field>
+          <button
+            className="auth-input bg-lwsGreen font-bold text-deepDark transition-all hover:opacity-90"
+            type="submit"
+          >
+            Log In
+          </button>
+        </Field>
+      </form>
     </>
   );
 };
